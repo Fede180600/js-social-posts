e7027d2bdcf8427c2f9d0d6ba8805de61c34ec90
Fede180600 <fede180600@libero.it>
2022-05-03 16:35:06 +0200

    create template literal

--- js/main.js
+++ js/main.js
@@ -104,6 +104,7 @@ const posts = [
 // Devo prelevare il Container dei posts
 const postsContainer = document.getElementById('container');
 
+//Richiamo la funzione per stampare i posts nel DOM 
 printPosts(container);
 
 
@@ -115,17 +116,48 @@ printPosts(container);
  * @param {element} postsContainer -> il container di tutti i posts
  * @returns {}
  */
-function printPosts(postsContainer) {
+function printPosts(container) {
     // // Ciclo forEach per scorrere tutti gli object dell'array dei post
     const printPost = posts.forEach(element => {
         // Creo il div post
         const post = document.createElement('div');
         // Aggiungo la classe post al div 
         post.classList.add('post');
-            // innerHTML per creare il template literal con la struttura del post
+        // innerHTML per creare il template literal con la struttura del post
+        post.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="https://unsplash.it/600/300?image=171" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>
+        `
+        // Appendo il div al container dei posts
+        postsContainer.append(post);
 
     })
-    return printPost
+    return printPost;
 }
 
-// Appendo il div al container dei posts
